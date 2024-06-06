d776574e5c0c19a88d69935848870f22f053b4d9
Siddhesh Dupare <siddheshdupare.sd2004@gmail.com>
2024-06-06 16:30:27 +0530

    Created all pages

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ class CustomHeader extends HTMLElement {
         <nav class="navigation-bar">
             <a href="index.html">Home</a>
             <a href="about.html">About</a>
-            <a href="#">Projects</a>
-            <a href="#">Contact</a>
+            <a href="projects.html">Projects</a>
+            <a href="contact.html">Contact</a>
         </nav>
         <!-- Navigation end -->
     </header>
@@ -21,3 +21,22 @@ class CustomHeader extends HTMLElement {
 }
 
 customElements.define('custom-header', CustomHeader);
+
+class CustomFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="footer-container">
+        <p class="footer-text">connect with us on social media</p>
+        <nav class="footer-navigation">
+            <a href="#"><img src="images/facebook-icon.png" alt="Facebook"/></a>
+            <a href="#"><img src="images/github-icon.png" alt="Github"/></a>
+            <a href="#"><img src="images/linkedin-icon.png" alt="LinkedIn"/></a>
+            <a href="#"><img src="images/instagram-icon.png" alt="Instagram"/></a>
+            <a href="#"><img src="images/twitter-icon.png" alt="Twitter"/></a>
+        </nav>
+    </footer>
+    `
+  }
+}
+
+customElements.define('custom-footer', CustomFooter);
